a999feaa850a71629d9ce13fcd5fb8faa8d95ac1
No17fox <buptwanglei@163.com>
2018-09-25 18:10:47 +0800

    write my code

--- main/main.js
+++ main/main.js
@@ -1,18 +1,49 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+o) 最大值 = ${sequence.maximun()}
+o) 元素数量 = ${sequence.len}
+o) 平均值 = ${sequence.average()}`
+);
 };
 
 class Sequence {
   constructor(input) {
     // Write your code here
+	this.input = input;
+	this.len = input.length;
+	this.len = input.length;
   }
 
   minimum() {
     // Write your code here
+	var min = this.input[0];
+	for (var i=1; i<this.len; i++) {
+		if (min > this.input[i]) {
+			min = this.input[i];
+		}
+	}
+	return min;
   }
 
   // Write your code here
+  maximun() {
+	var max = this.input[0];
+	for (var i=1; i<this.len; i++) {
+		if (max < this.input[i]) {
+			max = this.input[i];
+		}
+	}
+	return max;
+  }
+  
+  average() {
+	var sum = this.input[0];
+	for (var i=1; i<this.len; i++) {
+		sum = sum + this.input[i];	
+	}
+	var ave = Math.round(sum / this.len * 100) / 100;
+	return ave;
+  }
 }
+
